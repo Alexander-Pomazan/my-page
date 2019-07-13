426bd447e60b214e99ecb0a0f71318e58e758fc6
AlexOuttaspace <matanforeveryone@gmail.com>
2019-07-14 02:17:22 +0300

    fix typos

--- src/components/about-me.js
+++ src/components/about-me.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { styled } from 'linaria/react'
 
 import { TabHeader, Paragraph } from 'src/components'
 
@@ -8,15 +7,15 @@ export const AboutMe = () => (
     <TabHeader>About me</TabHeader>
 
     <Paragraph>
-      My main focus as a developer is to web apps with  beautiful interface and
-      smooth 60fps performance. I am  persistent when it comes to solving
-      difficult tasks and always ready to learn a new skill. 
+      My main focus as a developer is to create web apps with beautiful
+      interface and smooth 60fps performance. I am  persistent when it comes to
+      solving difficult tasks and always ready to learn a new skill. 
     </Paragraph>
 
     <Paragraph>
-      When working with a team, I always strive for creating an environment
-      where initiative and desire to create a perfect product is fostered and
-      hard effort is appreciated and rewarded. 
+      When working with team, I always strive for creating an environment where
+      initiative and desire to create a perfect product is fostered and hard
+      effort is appreciated and rewarded. 
     </Paragraph>
 
     <Paragraph>
